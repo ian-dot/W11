4db5d6fd2fbfe6bd6f50b17091b792f0eea45afe
Ian <egian93@gmail.com>
2021-12-08 01:00:48 -0600

    server.js created

---
+++ server.js
@@ -0,0 +1,22 @@
+// DEPENDENCIES
+const express = require('express');
+const apiRoutes = require('./routes/apiRoutes')
+const htmlRoutes = require('./routes/htmlRoutes')
+
+const app = express();
+
+//PORT
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'))
+
+// ROUTER
+app.use('/api', apiRoutes)
+app.use('/', htmlRoutes)
+
+// LISTENER
+app.listen(PORT, () => {
+  console.log(`App listening on PORT: ${PORT}`);
+});
